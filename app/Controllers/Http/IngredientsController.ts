f7ce4540c34806ab1e7de70c1bdb3b50066a5fce
mconstantine <mauro.constantinescu@gmail.com>
2021-11-22 15:25:49 +0100

    Indexing and showing ingredients.

--- app/Controllers/Http/IngredientsController.ts
+++ app/Controllers/Http/IngredientsController.ts
@@ -1,15 +1,39 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Ingredient from 'App/Models/Ingredient'
 
 export default class IngredientsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const query = request.input('query') || ''
+    const page = request.input('page') || 1
+    const perPage = request.input('perPage') || 20
 
-  public async create({}: HttpContextContract) {}
+    let match = Ingredient.query().select('id', 'name')
+
+    if (query) {
+      match = match.where('name', 'like', `%${query}%`)
+    }
+
+    return await match.paginate(page, perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async show({ request }: HttpContextContract) {
+    const id = request.param('id')
 
-  public async edit({}: HttpContextContract) {}
+    const ingredient = await Ingredient.query()
+      .preload('ranges', query => query.preload('unit'))
+      .where('id', id)
+      .firstOrFail()
+
+    return ingredient.serialize({
+      relations: {
+        ranges: {
+          fields: ['id', 'amount'],
+        },
+      },
+    })
+  }
 
   public async update({}: HttpContextContract) {}
 
