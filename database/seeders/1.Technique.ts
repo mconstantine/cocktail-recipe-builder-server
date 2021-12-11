ab73674680d0948e5389e8c8693fc55bdb30e9fd
mconstantine <mauro.constantinescu@gmail.com>
2021-12-11 17:43:16 +0100

    Using final ranges.

--- database/seeders/1.Technique.ts
+++ database/seeders/1.Technique.ts
@@ -28,50 +28,50 @@ export default class TechniqueSeeder extends BaseSeeder {
     ])
 
     await built.related('ranges').createMany([
-      { unitId: oz.id, min: 2.33, max: 2.5 },
-      { unitId: abv.id, min: 34, max: 40 },
-      { unitId: sugar.id, min: 9.5, max: 9.5 },
+      { unitId: oz.id, min: 2.9, max: 3.1 },
+      { unitId: abv.id, min: 27, max: 32 },
+      { unitId: sugar.id, min: 7, max: 8 },
       { unitId: acid.id, min: 0, max: 0 },
-      { unitId: dilution.id, min: 24, max: 24 },
+      { unitId: dilution.id, min: 23, max: 25 },
     ])
 
     await stirred.related('ranges').createMany([
-      { unitId: oz.id, min: 3, max: 3.25 },
-      { unitId: abv.id, min: 29, max: 43 },
-      { unitId: sugar.id, min: 5.3, max: 8 },
-      { unitId: acid.id, min: 0.15, max: 0.2 },
+      { unitId: oz.id, min: 4.33, max: 4.75 },
+      { unitId: abv.id, min: 21, max: 29 },
+      { unitId: sugar.id, min: 3.7, max: 5.6 },
+      { unitId: acid.id, min: 0.1, max: 0.14 },
       { unitId: dilution.id, min: 41, max: 49 },
     ])
 
     await shaken.related('ranges').createMany([
-      { unitId: oz.id, min: 3.25, max: 3.75 },
-      { unitId: abv.id, min: 23, max: 31.5 },
-      { unitId: sugar.id, min: 8, max: 13.5 },
-      { unitId: acid.id, min: 1.2, max: 1.4 },
+      { unitId: oz.id, min: 5.2, max: 5.9 },
+      { unitId: abv.id, min: 15, max: 19.7 },
+      { unitId: sugar.id, min: 5, max: 8.9 },
+      { unitId: acid.id, min: 0.76, max: 0.94 },
       { unitId: dilution.id, min: 51, max: 60 },
     ])
 
     await shakenWithEgg.related('ranges').createMany([
-      { unitId: oz.id, min: 4.33, max: 4.75 },
-      { unitId: abv.id, min: 18, max: 23 },
-      { unitId: sugar.id, min: 10, max: 13.2 },
-      { unitId: acid.id, min: 0.73, max: 1 },
+      { unitId: oz.id, min: 6.66, max: 7 },
+      { unitId: abv.id, min: 12.1, max: 15.2 },
+      { unitId: sugar.id, min: 6.7, max: 9 },
+      { unitId: acid.id, min: 0.49, max: 0.68 },
       { unitId: dilution.id, min: 46, max: 49 },
     ])
 
     await blended.related('ranges').createMany([
-      { unitId: oz.id, min: 0.75, max: 0.75 },
-      { unitId: abv.id, min: 28.6, max: 32.8 },
-      { unitId: sugar.id, min: 15, max: 15.4 },
-      { unitId: acid.id, min: 1.08, max: 1.09 },
+      { unitId: oz.id, min: 5.25, max: 5.25 },
+      { unitId: abv.id, min: 15, max: 17.2 },
+      { unitId: sugar.id, min: 7.9, max: 8.1 },
+      { unitId: acid.id, min: 0.5, max: 0.6 },
       { unitId: dilution.id, min: 90, max: 90 },
     ])
 
     await carbonated.related('ranges').createMany([
-      { unitId: oz.id, min: 3.12, max: 3.31 },
-      { unitId: abv.id, min: 8.75, max: 10.6 },
-      { unitId: sugar.id, min: 3.12, max: 4.97 },
-      { unitId: acid.id, min: 0.24, max: 0.34 },
+      { unitId: oz.id, min: 5, max: 5 },
+      { unitId: abv.id, min: 14, max: 16 },
+      { unitId: sugar.id, min: 5, max: 7.5 },
+      { unitId: acid.id, min: 0.38, max: 0.51 },
       { unitId: dilution.id, min: 51, max: 60 },
     ])
   }
