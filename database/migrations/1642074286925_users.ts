e521a411ee6bb546a5f3b5850f020e6de5b2c7a4
mconstantine <mauro.constantinescu@gmail.com>
2022-01-13 13:12:33 +0100

    Added name field to User.

--- database/migrations/1642074286925_users.ts
+++ database/migrations/1642074286925_users.ts
@@ -4,8 +4,9 @@ export default class UsersSchema extends BaseSchema {
   protected tableName = 'users'
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
+    this.schema.createTable(this.tableName, table => {
       table.increments('id').primary()
+      table.string('name', 255).notNullable()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
